752b91231f048baf46d5a5c5766e8afa6cedc76f
chirantar <gupta.chirantar@gmail.com>
2024-03-28 21:06:37 +0530

    Initial code.

---
+++ src/main/java/com/core/recipebox/RecipeboxApplication.java
@@ -0,0 +1,13 @@
+package com.core.recipebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeboxApplication.class, args);
+	}
+
+}
